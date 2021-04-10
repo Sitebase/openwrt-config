146a0d05ec2145e01a5eac731ef46717060f6ceb
Wim Mostmans <wim@sitebase.be>
2021-04-10 10:06:10 +0200

    remove fixed ssh key to be used

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 ip="192.168.8.1"
-sshkey="openwrt.pub"
 logtag="sitebase"
 
 source .secrets
+
 
 mkdir -p dist/
 
@@ -13,21 +13,21 @@ do
 done
 
 # Copy required files to device
-scp -i "~/.ssh/openwrt" dist/*.config root@${ip}:/tmp/
-scp -i "~/.ssh/openwrt" .secrets root@${ip}:~/
-scp -i "~/.ssh/openwrt" .profile root@${ip}:~/
+scp dist/*.config root@${ip}:/tmp/
+scp .secrets root@${ip}:~/
+scp .profile root@${ip}:~/
 
-#ssh -i "~/.ssh/openwrt" root@${ip} "cat /tmp/${sshkey} > /etc/dropbear/authorized_keys"
-#ssh -i "~/.ssh/openwrt" root@${ip} "/etc/init.d/dropbear restart"
+#ssh root@${ip} "/etc/init.d/dropbear restart"
 
-#ssh -i "~/.ssh/openwrt" root@${ip} "opkg update && opkg install luci-app-simple-adblock luci-mod-rpc"
+#ssh root@${ip} "opkg update && opkg install luci-app-simple-adblock luci-mod-rpc"
 
-#ssh -i "~/.ssh/openwrt" root@${ip} "logger -t ${logtag} Update config"
+#ssh root@${ip} "logger -t ${logtag} Update config"
 
-ssh -i "~/.ssh/openwrt" root@${ip} "cat /tmp/common.dropbear.config | uci -m import dropbear && uci commit"
-ssh -i "~/.ssh/openwrt" root@${ip} "cat /tmp/common.wireless.config | uci -m import wireless && uci commit"
-ssh -i "~/.ssh/openwrt" root@${ip} "cat /tmp/common.dhcp.config | uci -m import dhcp && uci commit"
-ssh -i "~/.ssh/openwrt" root@${ip} "cat /tmp/common.system.config | uci -m import system && uci commit"
+ssh root@${ip} "cat /tmp/common.dropbear.config | uci -m import dropbear && uci commit"
+ssh root@${ip} "cat /tmp/common.wireless.config | uci -m import wireless && uci commit"
+ssh root@${ip} "cat /tmp/common.dhcp.config | uci -m import dhcp && uci commit"
+ssh root@${ip} "cat /tmp/common.system.config | uci -m import system && uci commit"
+ssh root@${ip} "cat /tmp/common.glconfig.config | uci -m import glconfig && uci commit"
 
-#ssh -i "~/.ssh/openwrt" root@${ip} "/etc/init.d/dnsmasq restart"
-#ssh -i "~/.ssh/openwrt" root@${ip} "/etc/init.d/odhcpd restart"
+#ssh root@${ip} "/etc/init.d/dnsmasq restart"
+#ssh root@${ip} "/etc/init.d/odhcpd restart"
