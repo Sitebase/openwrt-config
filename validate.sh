4974d308b853d80840c1107bb77f50fe13552d3f
Wim Mostmans <wim@sitebase.be>
2021-05-01 09:57:13 +0200

    add some extra logging

--- validate.sh
+++ validate.sh
@@ -1,7 +1,8 @@
 # This is to run validation in the workflow
-echo "validate $GITHUB_WORKSPACE"
+echo "start validate $GITHUB_WORKSPACE"
 for i in *.config; do
-    if ! uci show "$i" > /dev/null
+	echo "validate $i"
+    if ! uci show "${GITHUB_WORKSPACE}${i}" > /dev/null
         then echo "$i"
     fi
 done
