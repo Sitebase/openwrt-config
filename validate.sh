7d5e7a3589bc467f39c43bb65d2c3f4f26c4dbc8
Wim Mostmans <wim@sitebase.be>
2021-05-01 10:11:27 +0200

    test uci show

--- validate.sh
+++ validate.sh
@@ -1,6 +1,11 @@
 # This is to run validation in the workflow
+
+echo "Testing"
+uci show common.dhcp -P "${GITHUB_WORKSPACE}${i}"
+
 echo "start validate $GITHUB_WORKSPACE"
 for i in *.config; do
+	echo "-> ${GITHUB_WORKSPACE}${i}"
 	uci show "${GITHUB_WORKSPACE}${i}"
     #if ! uci show "${GITHUB_WORKSPACE}${i}" > /dev/null
         #then echo "$i"
