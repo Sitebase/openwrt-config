a86d6dc662a8027b46ae17828c82f272d0090d93
Wim Mostmans <wim@sitebase.be>
2021-05-01 10:02:33 +0200

    show validation output

--- validate.sh
+++ validate.sh
@@ -1,8 +1,8 @@
 # This is to run validation in the workflow
 echo "start validate $GITHUB_WORKSPACE"
 for i in *.config; do
-	echo "validate $i"
-    if ! uci show "${GITHUB_WORKSPACE}${i}" > /dev/null
-        then echo "$i"
-    fi
+	uci show "${GITHUB_WORKSPACE}${i}"
+    #if ! uci show "${GITHUB_WORKSPACE}${i}" > /dev/null
+        #then echo "$i"
+    #fi
 done
