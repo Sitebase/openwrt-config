15a64454d5f99e3809f712a2f8c7ab95b3f64d2e
Wim Mostmans <wim@sitebase.be>
2021-05-01 09:51:18 +0200

    fix validate script

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 # This is to run validation in the workflow
 echo "validate $GITHUB_WORKSPACE"
 for i in *.config; do
-    if ! uci show "${UCI_CONF}" > /dev/null
-        then echo "${UCI_CONF}"
+    if ! uci show "$i" > /dev/null
+        then echo "$i"
     fi
 done
