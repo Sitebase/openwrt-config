c4aa76e9c8f9766d1bf78181cca0634f09a579b5
Wim Mostmans <wim@sitebase.be>
2021-05-01 10:22:55 +0200

    try path fix

--- validate.sh
+++ validate.sh
@@ -1,14 +1,14 @@
 # This is to run validation in the workflow
 
 echo "Test all"
-uci show all -P "${GITHUB_WORKSPACE}"
+uci show all -P "${GITHUB_WORKSPACE}/"
 echo "Testing"
-uci show bla -P "${GITHUB_WORKSPACE}"
+uci show bla -P "${GITHUB_WORKSPACE}/"
 
 echo "start validate $GITHUB_WORKSPACE"
 for i in *.config; do
-	echo "-> ${GITHUB_WORKSPACE}${i}"
-	uci show "${GITHUB_WORKSPACE}${i}"
+	echo "-> ${GITHUB_WORKSPACE}/${i}"
+	uci show "${GITHUB_WORKSPACE}/${i}"
     #if ! uci show "${GITHUB_WORKSPACE}${i}" > /dev/null
         #then echo "$i"
     #fi
