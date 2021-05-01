5aa8ee016256e87dc4604d3c3bcc34cf89966d2e
Wim Mostmans <wim@sitebase.be>
2021-05-01 11:15:11 +0200

    clean up validate script

--- validate.sh
+++ validate.sh
@@ -1,17 +1,4 @@
-# This is to run validation in the workflow
-
-#echo "Test show bla"
-#uci -c "${GITHUB_WORKSPACE}" show bla
-
-#echo "Test import"
-#uci -m import bla && uci commit
-
-#echo "Show all"
-#uci -P "${GITHUB_WORKSPACE}/" show bla
-
-#echo "Test show dhcp"
-#uci -c "${GITHUB_WORKSPACE}" show dhcp
-
+# run validation in github workflow
 cd ${GITHUB_WORKSPACE}/config/
 for i in *; do
 	if ! uci -c "${GITHUB_WORKSPACE}/config" show "${i}" > /dev/null
