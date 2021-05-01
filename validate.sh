598be285da063ee4942db936937945108b8c7fc4
Wim Mostmans <wim@sitebase.be>
2021-05-01 11:11:14 +0200

    improve feedback

--- validate.sh
+++ validate.sh
@@ -15,8 +15,10 @@
 cd ${GITHUB_WORKSPACE}/config/
 for i in *; do
 	if ! uci -c "${GITHUB_WORKSPACE}/config" show "${i}" > /dev/null
-		then 
-			echo "invalid config -> $i"
-			exit 1
+	then 
+		echo "invalid config -> $i"
+		exit 1
+	else
+		echo "valid $i"
 	fi
 done
